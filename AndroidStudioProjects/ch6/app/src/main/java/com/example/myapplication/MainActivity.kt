fe47a6532989cf8d2de0453364352cb6b33d2b0c
Jangsung Park <okidu529@gmail.com>
2021-05-24 03:19:43 +0900

    6장 스톱워치
    2021.05.24

---
+++ AndroidStudioProjects/ch6/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.concurrent.timer
+
+
+class MainActivity : AppCompatActivity() {
+    private var time = 0
+    private var isRunning = false
+    private var timerTask: Timer? = null
+    private var lap = 1
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fab.setOnClickListener {
+            isRunning = !isRunning
+
+            if(isRunning)
+                start()
+            else
+                pause()
+        }
+        lapButton.setOnClickListener() {
+            recordLapTime()
+        }
+        resetFab.setOnClickListener() {
+            reset()
+        }
+    }
+    private fun start() {
+        fab.setImageResource(R.drawable.ic_baseline_pause_24)
+
+        timerTask = timer(period = 10) {
+            time++
+            val sec = time / 100
+            val milli = time % 100
+            runOnUiThread {
+                secTextView.text = "$sec"
+                milliTextView.text = "$milli"
+            }
+        }
+    }
+    private fun pause() {
+        fab.setImageResource((R.drawable.ic_baseline_play_arrow_24))
+        timerTask?.cancel()
+    }
+    private fun recordLapTime() {
+        val lapTime = this.time
+        val textView = TextView(this)
+        textView.text = "%lap Lap : ${lapTime / 100}.${lapTime % 100}"
+
+        lapLayout.addView(textView, 0)
+        lap++
+    }
+    private fun reset() {
+        timerTask?.cancel()
+
+        time = 0
+        isRunning = false
+        fab.setImageResource(R.drawable.ic_baseline_play_arrow_24)
+        secTextView.text = "0"
+        milliTextView.text = "00"
+    }
+}
